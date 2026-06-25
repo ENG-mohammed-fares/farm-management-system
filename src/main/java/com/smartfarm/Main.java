5b5cad867c86a9a1367affc08179bdc3200f10e0
mohammedfares82 <s12323073@stu.najah.edu>
2026-06-25 18:39:44 +0300

    2

---
+++ src/main/java/com/smartfarm/Main.java
@@ -0,0 +1,24 @@
+package com.smartfarm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Smart Farm Management");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
